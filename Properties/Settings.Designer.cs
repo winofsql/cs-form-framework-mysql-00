baf946c41b931038dfc9f05b652a76fb7e95f715
sworc <lightbox@nifty.com>
2021-08-12 13:43:27 +0900

    Update Settings.Designer.cs

--- Properties/Settings.Designer.cs
+++ Properties/Settings.Designer.cs
@@ -9,7 +9,7 @@
 //------------------------------------------------------------------------------
 
 
-namespace cs_form_framework_mysql_01.Properties
+namespace cs_form_framework_mysql_00.Properties
 {
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
